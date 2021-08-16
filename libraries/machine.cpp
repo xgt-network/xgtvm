a63ef490d908032034a444ecaadddb0cedbd39a5
Roger Jungemann <roger@teamsketchy.com>
2021-08-16 10:43:13 -0700

    Changes to build with xgt project

--- libraries/machine.cpp
+++ libraries/machine.cpp
@@ -231,7 +231,7 @@ namespace machine
     big_word va, vb, vc, vd, ve, vf, vg, vh, vi, vj, vk, vl, vm, vn, vo, vp, vq;
     stack_variant sv;
     signed_big_word sa, sb, sc;
-    size_t offset, dest_offset, length, end, code_size;
+    size_t offset, dest_offset, length, code_size;
     std::vector<word> contract_args;
     std::vector<word> ext_contract_code;
     std::vector<word>::const_iterator first, last;
@@ -465,7 +465,8 @@ namespace machine
         {
           sv = stack.front(); // addr
           stack.pop_front();
-          if (ss = boost::get<std::string>(&sv))
+          ss = boost::get<std::string>(&sv);
+          if (ss)
           {
             push_word( adapter.get_balance(*ss) );
           }
@@ -556,7 +557,8 @@ namespace machine
       case extcodesize_opcode:
         logger << "op extcodesize" << std::endl;
         sv = stack.front(); // addr
-        if (ss = boost::get<std::string>(&sv))
+        ss = boost::get<std::string>(&sv);
+        if (ss)
         {
           stack.pop_front();
           ext_contract_code = adapter.get_code_at_addr(*ss);
@@ -571,7 +573,8 @@ namespace machine
       case extcodecopy_opcode:
         logger << "op extcodecopy" << std::endl;
         sv = stack.front(); // addr
-        if (ss = boost::get<std::string>(&sv))
+        ss = boost::get<std::string>(&sv);
+        if (ss)
         {
           stack.pop_front();
           ext_contract_code = adapter.get_code_at_addr(*ss);
@@ -618,7 +621,8 @@ namespace machine
       case extcodehash_opcode:
         logger << "op extcodehash" << std::endl;
         sv = stack.front(); // addr
-        if (ss = boost::get<std::string>(&sv))
+        ss = boost::get<std::string>(&sv);
+        if (ss)
         {
           stack.pop_front();
           push_word( adapter.get_code_hash(*ss) );
@@ -2610,7 +2614,8 @@ namespace machine
         va = pop_word(); // energy
 
         sv = stack.front(); // addr
-        if (ss = boost::get<std::string>(&sv))
+        ss = boost::get<std::string>(&sv);
+        if (ss)
         {
           stack.pop_front();
           vb = pop_word(); // value
@@ -2706,7 +2711,8 @@ namespace machine
       case selfdestruct_opcode:
         logger << "op selfdestruct" << std::endl;
         sv = stack.front(); // addr
-        if (ss = boost::get<std::string>(&sv))
+        ss = boost::get<std::string>(&sv);
+        if (ss)
         {
           adapter.self_destruct(*ss);
         }
