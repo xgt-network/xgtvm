b3a7aac5567748cbdd0b29636a2d665b5ceb8201
Liam Noroian <liamnoroian@gmail.com>
2021-10-25 06:13:27 -0700

    adding helper function for current_opcode

--- libraries/machine.hpp
+++ libraries/machine.hpp
@@ -12,7 +12,6 @@
 
 namespace machine
 {
-
   typedef uint8_t word;
   typedef boost::multiprecision::uint256_t big_word;
   typedef boost::multiprecision::int256_t signed_big_word;
@@ -280,9 +279,8 @@ namespace machine
     std::deque<stack_variant> stack;
     machine_state state = machine_state::running;
     context ctx;
-    message msg;
-    opcode current_opcode;
     std::vector<word> code;
+    message msg;
     std::map<size_t, word> memory;
     std::map<big_word, big_word> storage;
     std::vector<word> return_value;
@@ -291,6 +289,7 @@ namespace machine
     std::stringstream logger;
     chain_adapter adapter;
     big_word energy_left;
+    opcode current_opcode = stop_opcode;
 
     void push_word(stack_variant v);
     big_word pop_word();
@@ -311,6 +310,7 @@ namespace machine
     machine_state get_state();
     boost::optional<std::string> get_error_message();
     std::stringstream& get_logger();
+    opcode get_current_opcode() { return current_opcode; };
     std::string to_json();
 
     void emit_log(const log_object& o);
